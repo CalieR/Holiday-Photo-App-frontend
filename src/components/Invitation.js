420abc664380a527778ee53ecd1a045ccd5e8515
CalieR <calielrushton@gmail.com>
2019-05-20 18:13:18 +0100

    user can add another user to an album

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,12 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import api from "../util/api";
+// import { Dropdown } from "semantic-ui-react";
 
-class Invitation
- extends Component {
-    state = {  }
-    render() { 
-        return ( <p>Put a dropdown here which shows users not in the album yet.  Create a new album user on selection</p> );
-    }
+class Invitation extends Component {
+  state = {
+    users: [],
+    selectedUser: ""
+  };
+
+  // need to code this to take current album id
+  componentDidMount() {
+    api.inviteUsers(1).then(data => {
+      let usersFromAPI = data.map(user => {
+        return { value: user.id, display: user.username };
+      });
+      this.setState({
+        users: usersFromAPI
+      });
+    });
+  }
+
+  handleClick = e => {
+    e.preventDefault();
+    // album_id currently hardcoded, update to current album
+    api.addUserToAlbum(parseInt(this.state.selectedUser, 10), 1);
+    this.setState({
+      selectedUser: ""
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <select
+          value={this.state.selectedUser}
+          onChange={e => this.setState({ selectedUser: e.target.value })}
+        >
+          {this.state.users.map(user => (
+            <option key={user.value} value={user.value}>
+              {user.display}
+            </option>
+          ))}
+        </select>
+        <button onClick={this.handleClick}>Add user</button>
+      </div>
+    );
+  }
 }
- 
-export default Invitation
-;
+
+export default Invitation;
