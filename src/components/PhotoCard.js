86464c42ababbcee0e077e715a4cef0e7ffb5438
CalieR <calielrushton@gmail.com>
2019-05-24 14:51:07 +0100

    can show an individual photo in a modal

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import { Card, Image } from "semantic-ui-react";
+import { Card, Image, Button, Modal} from "semantic-ui-react";
 
+// add a modal to this component to expand the image
+// if that works, consider side scrolling for all images in an album
 class PhotoCard extends Component {
-  state = {};
   render() {
     return (
       <Card>
@@ -14,9 +15,14 @@ class PhotoCard extends Component {
         />
         <Card.Content>
           <Card.Header>{this.props.photo.title}</Card.Header>
-
           <Card.Description>{this.props.photo.description}</Card.Description>
         </Card.Content>
+        <Modal trigger={<Button>Show Full Size</Button>} closeIcon centered className="modal-overlay">
+          <Modal.Header>{this.props.photo.title}</Modal.Header>
+          <Modal.Content image>
+            <Image src={this.props.photo.image_url} className="modal-img"/>
+          </Modal.Content>
+        </Modal>
       </Card>
     );
   }
